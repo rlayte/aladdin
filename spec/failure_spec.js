7843248a3ec8c24a25b7fac9291d870ff2c29de7
Rich Layte <rich.layte@gmail.com>
2012-08-29 17:02:42 +0100

    add trace option

--- spec/failure_spec.js
+++ spec/failure_spec.js
@@ -3,4 +3,8 @@ describe("failure", function() {
   it ("should fail for the example", function() {
     expect(false).toBeTruthy();
   });
+
+  it('should fail if dependency is not loaded', function() {
+    expect(foo).toBeDefined();
+  });
 });
