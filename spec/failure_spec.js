38ee8980f6753b17385cddf7f152d8ead6eb6e06
Rich Layte <rich.layte@gmail.com>
2012-09-02 14:21:56 +0100

    correct indent level for failures over 10

--- spec/failure_spec.js
+++ spec/failure_spec.js
@@ -11,4 +11,28 @@ describe("failure", function() {
   it('should fail if function is invalid', function() {
     expect(foo.bar()).toBeTruthy();
   });
+
+  it ("should fail for the example", function() {
+    expect(false).toBeTruthy();
+  });
+
+  it('should fail if dependency is not loaded', function() {
+    expect(bar).toBeDefined();
+  });
+
+  it('should fail if function is invalid', function() {
+    expect(foo.bar()).toBeTruthy();
+  });
+
+  it ("should fail for the example", function() {
+    expect(false).toBeTruthy();
+  });
+
+  it('should fail if dependency is not loaded', function() {
+    expect(bar).toBeDefined();
+  });
+
+  it('should fail if function is invalid', function() {
+    expect(foo.bar()).toBeTruthy();
+  });
 });
